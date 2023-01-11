9d5e48dba48193aafe1a6f64cd3fe51a8787ac3c
Galiger Gergő <softengubb@gmail.com>
2023-01-11 12:02:30 +0100

    Corrected error in last line.

--- app.py
+++ app.py
@@ -1,3 +1,7 @@
+from flask import Flask, render_template
+import os
+import random
+
 app = Flask(__name__)
 
 # list of cat images
@@ -25,4 +29,4 @@ def index():
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
 
-x = 'dummy
+x = 'dummy'
